468c1626857f025bb2c84f08905b5203823114d2
Alberto <betim009@gmail.com>
2025-08-23 16:25:52 -0300

    update

--- material_aovivo/myapp/src/app/page.tsx
+++ material_aovivo/myapp/src/app/page.tsx
@@ -1,10 +1,43 @@
+"use client"
 import Header from "@/components/header"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [nome, setNome] = useState("Alberto")
+  const [quantidade, setQuantidade] = useState(1)
+
+  useEffect(() => {
+    // Quando a pagina carregar, vai executar esse bloco aqui.
+
+    alert("Ola")
+  }, [])
+
+  function handleChange({ target }: any) {
+    const { value } = target;
+    setNome(value)
+  }
+
+  function handleClick() {
+    if (quantidade + 1 >= 10) {
+      return null;
+    }
+    setQuantidade(quantidade + 1)
+  }
+
   return (
     <>
       <Header />
-      <h1>Ola</h1>
+      <h1>Ola {nome}</h1>
+      <input
+        type="text"
+        value={nome}
+        onChange={handleChange}
+      />
+      <p>{quantidade}</p>
+      <button
+        onClick={handleClick}>
+        +
+      </button>
     </>
   );
 }
